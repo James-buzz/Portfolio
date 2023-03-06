45e64c27ec3d3523b402ea1eb5c2227698ce5322
James <jameslomax96@gmail.com>
2023-03-06 17:56:31 +0000

    feat: enable vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.scss';
 import '@/styles/highlight.scss';
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 import NextNProgress from 'nextjs-progressbar';
 
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <NextNProgress options={{ showSpinner: false }} color="#CACACA" />
       <Component {...pageProps} />;
+      <Analytics />
     </>
   );
 }
